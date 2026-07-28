c33f110ac9afe0c3d8e2a02ab2a4e2e8b24e4219
Suiren91 <rento8191@gmail.com>
2026-07-28 16:22:36 +0900

    feat(service): add add_task service & refactor add_task handler to use add_task service

--- internal/handler/add_task.go
+++ internal/handler/add_task.go
@@ -5,14 +5,11 @@ import (
 	"net/http"
 
 	"github.com/Suiren91/GoExampleWebApp/internal/entity"
-	"github.com/Suiren91/GoExampleWebApp/internal/store"
 	"github.com/go-playground/validator/v10"
-	"github.com/jmoiron/sqlx"
 )
 
 type AddTask struct {
-	DB        *sqlx.DB
-	Repo      *store.Repository
+	Service   AddTaskService
 	Validator *validator.Validate
 }
 
@@ -34,11 +31,7 @@ func (at *AddTask) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}, http.StatusBadRequest)
 		return
 	}
-	t := &entity.Task{
-		Title:  b.Title,
-		Status: entity.TaskStatusTodo,
-	}
-	err = at.Repo.AddTask(ctx, at.DB, t)
+	t, err := at.Service.AddTask(ctx, b.Title)
 	if err != nil {
 		RespondJSON(ctx, w, ErrResponse{
 			Message: err.Error(),
